011a546e0dc14f9ed5dfb5724455451edcad9d7d
o06660o <2957531723@qq.com>
2025-05-28 08:41:01 +0800

    fix: remove debug message in `filesys/directory.c`

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -98,23 +98,6 @@ lookup (const struct dir *dir, const char *name, struct dir_entry *ep,
   ASSERT (dir != NULL);
   ASSERT (name != NULL);
 
-#define DEBUG
-#ifdef DEBUG
-  if (strcmp (name, "stuff") == 0)
-    {
-      size_t tmp = 0;
-      inode_read_at (dir->inode, &e, sizeof e, tmp);
-      tmp += sizeof e;
-      inode_read_at (dir->inode, &e, sizeof e, tmp);
-      tmp += sizeof e;
-      inode_read_at (dir->inode, &e, sizeof e, tmp);
-      tmp += sizeof e;
-      inode_read_at (dir->inode, &e, sizeof e, tmp);
-      tmp += sizeof e;
-      inode_read_at (dir->inode, &e, sizeof e, tmp);
-    }
-#endif
-
   for (ofs = 0; inode_read_at (dir->inode, &e, sizeof e, ofs) == sizeof e;
        ofs += sizeof e)
     if (e.in_use && !strcmp (name, e.name))
